c3d57325d6ede68295d1b660ea852c8f5a5a5420
Sávio Mendes <saviomd@gmail.com>
2016-02-09 20:24:46 -0200

    Update bootstrap and other dependencies

--- js/driblesfifa.js
+++ js/driblesfifa.js
@@ -1,7 +1,7 @@
 var driblesfifa = driblesfifa || {};
 
 driblesfifa.templates = (function () {
-	var tutorial = '<li class="m-b animated fadeInRight">' +
+	var tutorial = '<li class="m-b-1 animated fadeInRight">' +
 			'<a class="embed-responsive embed-responsive-4by3 js-link-tutorial" href="https://www.youtube.com/watch?v={{videoId}}" data-title="{{title}}" target="_blank">' +
 				'<img alt="{{title}}" class="embed-responsive-item" src="{{thumbnail}}">' +
 			'</a>' +
